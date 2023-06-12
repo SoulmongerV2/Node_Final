df34fddef4a9d25b86ec12579cc705142b42293e
nguq03 <nguq03@vse.cz>
2023-06-13 00:27:36 +0200

    base test

---
+++ tests/messages.spec.js
@@ -0,0 +1,21 @@
+import test from "ava"
+import supertest from "supertest"
+import { db } from "../src/database.js"
+import { app } from "../src/app.js"
+
+test.beforeEach(async () => {
+    await db.migrate.latest()
+  })
+  
+test.afterEach.always(async () => {
+await db.migrate.rollback()
+})
+
+
+test.serial('GET / shows list of todos', async (t) => {
+await db("messages").insert({ text: "Message test!!" })
+
+const response = await supertest(app).get('/')
+
+t.assert(response.text.includes("Message test!!"))
+})
