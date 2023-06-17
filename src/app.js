857af2b80bd10f71a6f6cdc0e1ea03c143470d3e
nguq03 <nguq03@vse.cz>
2023-06-17 20:40:43 +0200

    middleware refactor - loaderUser, requireAuth

--- src/app.js
+++ src/app.js
@@ -1,19 +1,12 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import { db } from "./database.js"
-import {  getUserByToken } from "../src/database/users.js"
+
 import { router as messagesRouter } from "./routes/messages.js"
 import { router as usersRouter } from "./routes/users.js" 
-
+import loadUser from "./middlewares/loadUser.js"
+import requireAuth from "./middlewares/requireAuth.js"
 export const app = express()
-
-const auth = (req, res, next) => {
-    if (res.locals.user) {
-        next()
-    } else {
-        res.redirect("/register")
-    }
-}
 
 
 app.set("view engine", "ejs")
@@ -22,20 +15,10 @@ app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-app.use(async (req, res, next) => {
-    const token = req.cookies.token
-
-    if(token) {
-        res.locals.user = await getUserByToken(token)
-    } else {
-        res.locals.user = null
-    }
-
-    next()
-})
+app.use(loadUser)
 
 
-app.get("/", async (req, res) => {
+app.get("/", requireAuth, async (req, res) => {
 
     const messages = await db("messages").select("*")
 
---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,9 @@
+
+
+export default (req, res, next) => {
+    if (res.locals.user) {
+        next()
+    } else {
+        res.redirect("/register")
+    }
+}
